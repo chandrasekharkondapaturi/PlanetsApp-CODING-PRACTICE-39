4af6f023f42e2213492c7f0a54d87aedf0a6f2da
chandrasekharkondapaturi <chandu.mc87@gmail.com>
2023-02-09 04:19:01 +0000

    planetsApp

---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,24 @@
+// Write your code here
+import Slider from 'react-slick'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+
+  return (
+    // eslint-disable-next-line react/no-unknown-property
+    <div className="planets-app-container" testid="planets">
+      <h1 className="heading">PLANETS</h1>
+      <Slider>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
